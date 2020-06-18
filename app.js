6120a173845e2f19747f674ef1fdccbdcc95c060
PATRA <APATRA@Contractor.lb.com>
2020-06-18 20:21:38 +0530

    last commit

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const dbName = 'avijittask';
 MongoClient.connect(url, function(err, client) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
-
   const db = client.db(dbName);
   taskOne(db, function() {
       taskTwo(db, function(){
@@ -32,29 +31,15 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 
-
 const taskOne = function(db, callback) {
     console.log("call task One..");
-
-    // Get the documents collection
-    // const collection = db.collection('documents');
-    // // Insert some documents
-    // collection.insertMany([
-    //   {a : 1}, {a : 2}, {a : 3}
-    // ], function(err, result) {
-    //   assert.equal(err, null);
-    //   assert.equal(3, result.result.n);
-    //   assert.equal(3, result.ops.length);
-    //   console.log("Inserted 3 documents into the collection");
-    //   callback(result);
-    // });
-
-
-    // db.collname.aggregate([
-    //     {"$group":{
-    //         "_id":"$awards","noawards":{"$sum": 1}
-    //         }}
-    //     ])
+    
+    db.authorsDetails.aggregate([
+        {"$group":{
+            _id: { "$gte":[ "$awards", n ]
+            }}
+        }
+        ])
     callback();
   }
 
@@ -70,28 +55,38 @@ const taskTwo = function(db,callback){
     //         "year":{"$gte":"awards"}
     //         }}
     //     ])
+
+    db.author.find( { author: { $gt: new Date('1950-01-01') } } )
         
     callback();
 }
 const taskThree = function(db,callback){
     console.log("call task three");
+    db.booksDeatils.aggregate([
+        {$unwind:"$statuses"},
+        {$group:{"_id":"$_id",
+                "sold":{$last:"$statuses.sold"},
+                "seller_id":{$last:"$seller_id"}}},
+        {$group:{"_id":"$seller_id",
+                "total_sold":{$sum:"$sold"}},
+                totalSales:{$sum:"$sold.Product_price"}},
+        {$sort:{"$total_sold":-1}}
+    ])
     callback();
 
 }
 const taskFour = function(db,callback){
     console.log("call task four");
-
-    // db.collname.aggregate([
-    //     {"$project":{
-    //     "authors": 1,
-    //     "DOB": 1,
-    //     "books": 1
-    //     }},
-    //     {"$match":{"dob":{"$gte":{"input given DOB"}}}},
-    //       {"$group":{
-    //         "_id":"$authors","totalPrice":{"$sum": 1}
-    //         }}  
-    //      ])
-
+    db.collname.aggregate([
+        {"$project":{
+        "authors": 1,
+        "DOB": 1,
+        "books": 1
+        }},
+        {"$match":{"dob":{"$gte":{"input given DOB"}}}},
+          {"$group":{
+            "_id":"$authors","totalPrice":{"$sum": $product.product_price}
+            }}  
+         ])
     callback();
 }
